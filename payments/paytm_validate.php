6c9a2ead94b3d717ea4e73917fd4835a9aaf8e22
Abhishek Goyal <abhishek.goyal@one97.delhi.net>
2017-04-12 14:34:31 +0530

    Key Encoding Change

--- payments/paytm_validate.php
+++ payments/paytm_validate.php
@@ -19,16 +19,20 @@
 			// Create an array having all required parameters for status query.
 			$requestParamList = array("MID" => get_setting_value($payment_params, "merchant_id", "") , "ORDERID" => $_POST['ORDERID']);
 			
+			$StatusCheckSum = getChecksumFromArray($requestParamList, get_setting_value($payment_params, "merchant_key", ""));
+							
+			$requestParamList['CHECKSUMHASH'] = $StatusCheckSum;
+			
 			// Call the PG's getTxnStatus() function for verifying the transaction status.
 			if(get_setting_value($payment_params, "live_mode", "") == 'yes')
 			{
-				$check_status_url = 'https://secure.paytm.in/oltp/HANDLER_INTERNAL/TXNSTATUS';
+				$check_status_url = 'https://secure.paytm.in/oltp/HANDLER_INTERNAL/getTxnStatus';
 			}
 			else
 			{
-				$check_status_url = 'https://pguat.paytm.com/oltp/HANDLER_INTERNAL/TXNSTATUS';
+				$check_status_url = 'https://pguat.paytm.com/oltp/HANDLER_INTERNAL/getTxnStatus';
 			}
-			$responseParamList = callAPI($check_status_url, $requestParamList);
+			$responseParamList = callNewAPI($check_status_url, $requestParamList);
 			if($responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$_POST["TXNAMOUNT"])
 			{			
 				$success_message = $_POST['RESPMSG'];
