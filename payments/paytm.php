49fdd66996fc44beca8860a5a007bc8f404d1c14
Lalit Chaudhary <lalit3.kumar@paytm.com>
2021-01-19 10:33:34 +0530

    update js checkout plugin

--- payments/paytm.php
+++ payments/paytm.php
@@ -1,5 +1,9 @@
 <?php
-	include_once("../includes/encdec_paytm.php");
+    header("Pragma: no-cache");
+    header("Cache-Control: no-cache");
+    header("Expires: 0");
+	include_once("../includes/paytm/PaytmChecksum.php");
+	include_once("../includes/paytm/PaytmHelper.php");
 	
 	$is_admin_path = true;
 	$root_folder_path = "../";
@@ -24,14 +28,20 @@
 	$pass_data = array(); 
 	$variables = array();
 		
+
+
+
+
 	get_payment_parameters($order_id, $payment_params, $pass_parameters, $post_parameters, $pass_data, $variables, "");
 
 	$checkSum = "";
 	$paramList = array();
-	$callback_url = get_setting_value($payment_params, "callback_url", "");
+	//$callback_url = get_setting_value($payment_params, "callback_url", "");
+	$callback_url = 'http://127.0.0.1/viart/viart56/payments/paytm_validate.php';
+	$merchant_key = get_setting_value($payment_params, "merchant_key", "");
 	
-	$ORDER_ID = $_POST["invoice"];
-	$CUST_ID = $_POST["email"];
+	$ORDER_ID = PaytmHelper::getPaytmOrderId($_POST["invoice"]);
+	$CUST_ID = trim($_POST["email"]);
 	$INDUSTRY_TYPE_ID = get_setting_value($payment_params, "industry", "");
 	$CHANNEL_ID = get_setting_value($payment_params, "channel", "");	
 	$email = $_POST["email"];
@@ -45,8 +55,37 @@
 	$paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
 	$paramList["WEBSITE"] = get_setting_value($payment_params, "merchant_website", "");
 	$paramList["CALLBACK_URL"] = $callback_url;
+
+
+
+
+	    $paytmParams["body"] = array(
+            "requestType" => "Payment",
+            "mid" => $paramList["MID"],
+            "websiteName" => $paramList["WEBSITE"],
+            "orderId" => $paramList["ORDER_ID"],
+            "callbackUrl" => $paramList["CALLBACK_URL"],
+            "txnAmount" => array(
+                "value" => $paramList["TXN_AMOUNT"],
+                "currency" => "INR",
+            ),
+            "userInfo" => array(
+                "custId" => $paramList["CUST_ID"],
+            ),
+        );
+
+        $generateSignature = PaytmChecksum::generateSignature(json_encode($paytmParams['body'], JSON_UNESCAPED_SLASHES), $merchant_key);
+        $paytmParams["head"] = array(
+            "signature" => $generateSignature
+        );
 	
-	$checkSum = getChecksumFromArray($paramList, get_setting_value($payment_params, "merchant_key", ""));
+
+//
+
+
+//print_r($paytmParams);  exit;
+   
+	//$checkSum = getChecksumFromArray($paramList, get_setting_value($payment_params, "merchant_key", ""));
 	/*	19751/17Jan2018	*/
 		/*if(get_setting_value($payment_params, "live_mode", "") == 'yes') {
 			$post_url = "https://secure.paytm.in/oltp-web/processTransaction";
@@ -54,35 +93,135 @@
 			$post_url = "https://pguat.paytm.com/oltp-web/processTransaction";
 		}*/
 
-		/*if(get_setting_value($payment_params, "live_mode", "") == 'yes') {
-			$post_url = "https://securegw.paytm.in/theia/processTransaction";
+		if(get_setting_value($payment_params, "live_mode", "") == 'yes') {
+			$paytm_env = 1;
 		} else {
-			$post_url = "https://securegw-stage.paytm.in/theia/processTransaction";
-		}*/
-		$post_url = get_setting_value($payment_params, "transaction_url", "");
+			$paytm_env = 0;
+		}
+
+
+		$apiURL = PaytmHelper::getPaytmURL(PaytmConstants::TRANSACTION_INIT_URL,$paytm_env).$paramList['MID'].'&ORDER_ID='.$paramList['ORDER_ID'];
+
+		        $post_data_string = json_encode($paytmParams, JSON_UNESCAPED_SLASHES);
+        $response_array = PaytmHelper::executecUrl($apiURL, $post_data_string);
+
+        if(!empty($response_array['body']['txnToken'])){
+        $txnToken = $response_array['body']['txnToken'];
+        $paytm_msg = PaytmConstants::TNX_TOKEN_GENERATED;
+        }else{
+         $txnToken = '';
+         $paytm_msg = PaytmConstants::RESPONSE_ERROR;
+
+        }
+
+ //print_r($response_array); exit;
+		//$post_url = get_setting_value($payment_params, "transaction_url", "");
 	/*	19751/17Jan2018 end	*/
+
+	//echo $post_url; 
+	//print_r($paramList); 
 	?>
 	<html>
 		<head>
 			<title>Merchant Check Out Page</title>
 		</head>
-		<body>
-			<center><h1>Please do not refresh this page...</h1></center>
-			<form method="post" action="<?php echo $post_url; ?>" name="f1">
-				<table border="1">
-					<tbody>			
-						<?php
-						foreach($paramList as $name => $value) {
-						echo '<input type="hidden" name="' . $name .'" value="' . $value . '">';
-						}
-						?>
-						<input type="hidden" name="CHECKSUMHASH" value="<?php echo $checkSum ?>">
-					</tbody>
-				</table>
-				<script type="text/javascript">
-				document.f1.submit();
-				</script>
-			</form>
+		<body class="">
+
+
+
+
+
+        <div id="paytm-pg-spinner" class="paytm-pg-loader ">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+            <div class="bounce4"></div>
+            <div class="bounce5"></div>
+        </div>
+        <div class="paytm-overlay"></div>
+
+        <script type="application/javascript" crossorigin="anonymous" src="<?php echo PaytmConstants::STAGING_HOST; ?>merchantpgpui/checkoutjs/merchants/<?php echo $paramList['MID']; ?>.js"></script>
+        
+        <div class="loader"></div>
+        <style type="text/css">
+            #paytm-pg-spinner {margin: 20% auto 0;width: 72px;text-align: center;z-index: 999999;position: relative;display: block;}
+
+            #paytm-pg-spinner > div {width: 10px;height: 10px;background-color: #012b71;border-radius: 100%;display: inline-block;-webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;animation: sk-bouncedelay 1.4s infinite ease-in-out both;}
+
+            #paytm-pg-spinner .bounce1 {-webkit-animation-delay: -0.64s;animation-delay: -0.64s;}
+
+            #paytm-pg-spinner .bounce2 {-webkit-animation-delay: -0.48s;animation-delay: -0.48s;}
+            #paytm-pg-spinner .bounce3 {-webkit-animation-delay: -0.32s;animation-delay: -0.32s;}
+
+            #paytm-pg-spinner .bounce4 {-webkit-animation-delay: -0.16s;animation-delay: -0.16s;}
+            #paytm-pg-spinner .bounce4, #paytm-pg-spinner .bounce5{background-color: #48baf5;} 
+            @-webkit-keyframes sk-bouncedelay {0%, 80%, 100% { -webkit-transform: scale(0) }40% { -webkit-transform: scale(1.0) }}
+
+            @keyframes sk-bouncedelay { 0%, 80%, 100% { -webkit-transform: scale(0);transform: scale(0); } 40% { 
+                                            -webkit-transform: scale(1.0); transform: scale(1.0);}}
+            .paytm-overlay{width: 100%;position: fixed;top: 0px;opacity: .4;height: 100%;background: #000;display: block;}
+            #errorDivPaytm {
+                color: red !important;
+            }
+
+        </style>
+
+        <script type="text/javascript">
+         
+
+                //$( '<div class="paytm-overlay paytm-pg-loader"></div>' ).insertBefore("body");
+                function openBlinkCheckoutPopup(orderId, txnToken, amount)
+                {
+                    var config = {
+                        "root": "",
+                        "flow": "DEFAULT",
+                        "data": {
+                            "orderId": orderId,
+                            "token": txnToken,
+                            "tokenType": "TXN_TOKEN",
+                            "amount": amount
+                        },
+                        "merchant": {
+                            "redirect": true
+                        },
+                        "handler": {
+                    
+                            "notifyMerchant": function (eventName, data) {
+                                
+                               if(eventName == 'SESSION_EXPIRED'){
+                                location.reload(); 
+                               }
+                            }
+                        }
+                    };
+                    if (window.Paytm && window.Paytm.CheckoutJS) {
+                        // initialze configuration using init method 
+                        window.Paytm.CheckoutJS.init(config).then(function onSuccess() {
+                            // after successfully updating configuration, invoke checkoutjs
+                            window.Paytm.CheckoutJS.invoke();
+
+                            //  $('.paytm-pg-loader').css("display", "none");
+
+                        }).catch(function onError(error) {
+                          //  console.log("error => ", error);
+                        });
+                    }
+                }
+
+                setTimeout(function(){ 
+
+                	 openBlinkCheckoutPopup('<?php echo $paramList['ORDER_ID']; ?>','<?php echo $txnToken; ?>','<?php echo $paramList['TXN_AMOUNT']; ?>'); 
+
+
+                }, 3000);
+                 
+
+ 
+
+        </script>
+
+
+
 		</body>
 	</html>
 	
--- payments/paytm_validate.php
+++ payments/paytm_validate.php
@@ -1,44 +1,79 @@
 <?php
-	include_once("./includes/encdec_paytm.php");
+
+	include_once("../includes/paytm/PaytmChecksum.php");
+	include_once("../includes/paytm/PaytmHelper.php");
 	$is_admin_path = true;
 	$root_folder_path = "../";
+	include_once ($root_folder_path ."includes/common.php");
+	include_once ($root_folder_path ."includes/order_items.php");
+	include_once ($root_folder_path ."messages/".$language_code."/cart_messages.php");
+	include_once ($root_folder_path ."includes/parameters.php");
 	
 	$paytmChecksum = "";
 	$paramList = array();
 	$isValidChecksum = "FALSE";
 
+
+
+	$vc = get_session("session_vc");
+	$order_id = get_session("session_order_id");
+	
+	$order_errors = check_order($order_id, $vc);
+	if($order_errors) {
+		echo $order_errors;
+		exit;
+	}
+
+	$post_parameters = ""; 
+	$payment_params = array(); 
+	$pass_parameters = array(); 
+	$pass_data = array(); 
+	$variables = array();
+
+
 	$paramList = $_POST;
 	$paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //Sent by Paytm pg
 
-	$isValidChecksum = verifychecksum_e($paramList, get_setting_value($payment_params, "merchant_key", ""), $paytmChecksum); 
-	if($isValidChecksum == "TRUE") 
-	{		
+    get_payment_parameters($order_id, $payment_params, $pass_parameters, $post_parameters, $pass_data, $variables, "");
+
+	$merchant_key = get_setting_value($payment_params, "merchant_key", "");
+	if(get_setting_value($payment_params, "live_mode", "") == 'yes') {
+	$paytm_env = 1;
+	}else{
+	$paytm_env = 0;
+	}
+
+	//$isValidChecksum = verifychecksum_e($paramList, get_setting_value($payment_params, "merchant_key", ""), $paytmChecksum); 
+    $isValidChecksum = PaytmChecksum::verifySignature($paramList, $merchant_key, $paytmChecksum);
+
+	if($isValidChecksum == "TRUE" || $isValidChecksum == "true" || $isValidChecksum == "1" || $isValidChecksum="01") 
+	{
+
 		if ($_POST["STATUS"] == "TXN_SUCCESS") 
 		{
 			// Create an array having all required parameters for status query.
-			$requestParamList = array("MID" => get_setting_value($payment_params, "merchant_id", "") , "ORDERID" => $_POST['ORDERID']);
-			
-			$StatusCheckSum = getChecksumFromArray($requestParamList, get_setting_value($payment_params, "merchant_key", ""));
-							
-			$requestParamList['CHECKSUMHASH'] = $StatusCheckSum;
-			
-			// Call the PG's getTxnStatus() function for verifying the transaction status.
-			/*	19751/17Jan2018	*/
-				/*if(get_setting_value($payment_params, "live_mode", "") == 'yes') {
-					$check_status_url = 'https://secure.paytm.in/oltp/HANDLER_INTERNAL/getTxnStatus';
-				} else {
-					$check_status_url = 'https://pguat.paytm.com/oltp/HANDLER_INTERNAL/getTxnStatus';
-				}*/
+			$requestParamList = array("MID" => $_POST['MID'] , "ORDERID" => $_POST['ORDERID']);
+	
+			$paytmParamsStatus = array();
 
-				/*if(get_setting_value($payment_params, "live_mode", "") == 'yes') {
-					$check_status_url = 'https://securegw.paytm.in/merchant-status/getTxnStatus';
-				} else {
-					$check_status_url = 'https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
-				}*/
-				$check_status_url = get_setting_value($payment_params, "transaction_status_url", "");
-			/*	19751/17Jan2018 end	*/
-			$responseParamList = callNewAPI($check_status_url, $requestParamList);
-			if($responseParamList['STATUS']=='TXN_SUCCESS' && $responseParamList['TXNAMOUNT']==$_POST["TXNAMOUNT"])
+            /* body parameters */
+            $paytmParamsStatus["body"] = array(
+                /* Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+                "mid" => $requestParamList['MID'],
+                /* Enter your order id which needs to be check status for */
+                "orderId" => $requestParamList['ORDERID'],
+            );
+            $checksumStatus = PaytmChecksum::generateSignature(json_encode($paytmParamsStatus["body"], JSON_UNESCAPED_SLASHES), $merchant_key);
+            /* head parameters */
+            $paytmParamsStatus["head"] = array(
+                /* put generated checksum value here */
+                "signature" => $checksumStatus
+            );
+            $post_data_status = json_encode($paytmParamsStatus, JSON_UNESCAPED_SLASHES);
+            $responseStatusArray = PaytmHelper::executecUrl(PaytmHelper::getPaytmURL(PaytmConstants::ORDER_STATUS_URL, 0), $post_data_status);
+
+
+			if($responseStatusArray['body']['resultInfo']['resultStatus'] == 'TXN_SUCCESS' && $responseStatusArray['body']['txnAmount'] == $_POST['TXNAMOUNT'])
 			{			
 				$success_message = $_POST['RESPMSG'];
 				// update order information
@@ -46,15 +81,15 @@
 				$sql  = " UPDATE " . $table_prefix . "orders ";
 				$sql .= " SET success_message=" . $db->tosql($_POST["STATUS"], TEXT);
 				$sql .= ", pending_message='', error_message='' ";
-				if ($transaction_id) {
+				if ($_POST["TXNID"]) {
 					$sql .= ", transaction_id=" . $db->tosql($_POST["TXNID"], TEXT);
 				}
-				$sql .= " WHERE order_id=" . $db->tosql($_POST["ORDERID"], INTEGER);
+				$sql .= " WHERE order_id=" . $db->tosql($order_id, INTEGER);
 				$db->query($sql);
 
 				// update order status
 				if ($_POST["STATUS"]) {
-					update_order_status($_POST["ORDERID"], $_POST["STATUS"], true, "", $status_error);
+					update_order_status($order_id, $_POST["STATUS"], true, "", $status_error);
 				}
 			}
 			else{
@@ -66,15 +101,15 @@
 				$sql .= " SET success_message=" . $db->tosql($status, TEXT);;
 				$sql .= ", pending_message='' ";
 				$sql .= ", error_message=" . $db->tosql($status, TEXT);;
-				if ($transaction_id) {
+				if ($_POST["TXNID"]) {
 					$sql .= ", transaction_id=" . $db->tosql($_POST["TXNID"], TEXT);
 				}
-				$sql .= " WHERE order_id=" . $db->tosql($_POST["ORDERID"], INTEGER);
+				$sql .= " WHERE order_id=" . $db->tosql($order_id, INTEGER);
 				$db->query($sql);
 				
 				// update order status
 				if ($status) {
-					update_order_status($_POST["ORDERID"], $status, true, "", $status_error);
+					update_order_status($order_id, $status, true, "", $status_error);
 				}
 			}
 		}
@@ -87,15 +122,15 @@
 			$sql .= " SET success_message=" . $db->tosql($_POST["STATUS"], TEXT);
 			$sql .= ", pending_message='' ";
 			$sql .= ", error_message=" . $db->tosql($_POST["STATUS"], TEXT);
-			if ($transaction_id) {
+			if ($_POST["TXNID"]) {
 				$sql .= ", transaction_id=" . $db->tosql($_POST["TXNID"], TEXT);
 			}
-			$sql .= " WHERE order_id=" . $db->tosql($_POST["ORDERID"], INTEGER);
+			$sql .= " WHERE order_id=" . $db->tosql($order_id, INTEGER);
 			$db->query($sql);
 			
 			// update order status
 			if ($_POST["STATUS"]) {
-				update_order_status($_POST["ORDERID"], $_POST["STATUS"], true, "", $status_error);
+				update_order_status($order_id, $_POST["STATUS"], true, "", $status_error);
 			}
 		}
 	}
@@ -108,14 +143,14 @@
 		$sql .= " SET success_message=" . $db->tosql($_POST["STATUS"], TEXT);
 		$sql .= ", pending_message='' ";
 		$sql .= ", error_message=" . $db->tosql($_POST["STATUS"], TEXT);
-		if ($transaction_id) {
+		if ($_POST["TXNID"]) {
 			$sql .= ", transaction_id=" . $db->tosql($_POST["TXNID"], TEXT);
 		}
-		$sql .= " WHERE order_id=" . $db->tosql($_POST["ORDERID"], INTEGER);
+		$sql .= " WHERE order_id=" . $db->tosql($order_id, INTEGER);
 		$db->query($sql);
 		// update order status
 		if ($_POST["STATUS"]) {
-			update_order_status($_POST["ORDERID"], $_POST["STATUS"], true, "", $status_error);
+			update_order_status($order_id, $_POST["STATUS"], true, "", $status_error);
 		}			
 		echo "<b>Checksum mismatched.</b>";
 		//Process transaction as suspicious.
